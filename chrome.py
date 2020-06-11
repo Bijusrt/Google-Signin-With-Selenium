45f85af8fdf5f7600d2406aee72ff3ad86c562ca
Bijusrt <biju19@navgurukul.org>
2020-06-11 16:13:37 +0530

    first commit

---
+++ chrome.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import time
+driver=webdriver.Chrome('chromedriver_linux64/chromedriver')
+driver.get('https://accounts.google.com/signin/v2/identifier?flowName=GlifWebSignIn&flowEntry=ServiceLogin')
+driver.maximize_window()
+a=driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input').send_keys('biju19@navgurukul.org')
+driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div').click()
+# a.submit() it will not work in this login page
+time.sleep(4)
+driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input').send_keys('yendaunakupola')
+driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div').click()
